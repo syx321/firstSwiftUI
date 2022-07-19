c94b1dd871c7cb0b67b7469ceea27fa0409ecd8a
syx321 <1580925976@qq.com>
2022-07-19 17:16:27 +0800

    Initial Commit

---
+++ firstSwiftUI/firstSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  firstSwiftUIApp.swift
+//  firstSwiftUI
+//
+//  Created by 苏易肖 on 2022/7/19.
+//
+
+import SwiftUI
+
+@main
+struct firstSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
